5ae1acdc5774692507965c02d466d173d63c9409
Anastasia Beloded <nastenabeloded@gmail.com>
2022-08-20 13:18:16 +0300

    new

---
+++ src/test/java/SwipeTests.java
@@ -0,0 +1,14 @@
+import config.AppiumConfiguration;
+import org.testng.annotations.Test;
+import screens.DragScreen;
+
+public class SwipeTests extends AppiumConfiguration {
+    @Test
+    public void swipeTest() {
+        new DragScreen(driver)
+                .selectSwipe()
+                .selectSwipeBasic();
+                //.swipeFromLeftToRight(2)
+               // .swipeFromRightToLeft(4);
+    }
+}
